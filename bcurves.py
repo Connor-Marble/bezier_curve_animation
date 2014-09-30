39ec9ea6be3e5b3f80da659f085d58024023f6d3
Connor-Marble <MarbleConnor@gmail.com>
2014-09-29 20:34:02 -0400

    added basic curve drawing class and init

--- bcurves.py
+++ bcurves.py
@@ -1,35 +1,35 @@
 
-class 4PointCurve():
-    def init(self, pointA, pointB, PointC, pointD):
+class FourPointCurve:
+    def __init__(self, pointA, pointB, pointC, pointD):
         self.pointA = pointA
         self.pointB = pointB
         self.pointC = pointC
         self.pointD = pointD
 
-    def lerp(a, b, t):
+    def lerp(self, a, b, t):
 
         if not len(a) == len(b):
             raise error('Mismatched point dimensions')
 
         result = []
 
-        for i in a:
+        for i in range(len(a)):
             result.append(a[i] + (b[i]-a[i])*t)
 
         return result
 
     def getcurvepoint(self, t):
-        ABpoint = lerp(self.pointA, self.pointC, t)
-        CDpoint = lerp(self.PointC, self.pointD, t)
-        curvepoint = lerp(ABpoint, CDpoint, t)
+        ABpoint = self.lerp(self.pointA, self.pointC, t)
+        CDpoint = self.lerp(self.pointC, self.pointD, t)
+        curvepoint = self.lerp(ABpoint, CDpoint, t)
 
         return curvepoint
         
 
-class 3PointCurve():
+class ThreePointCurve:
 
-    def init(self, pointA, pointB, pointC):
-        self.curve = 4PointCurve(pointA, pointB, pointB, pointC)
+    def __init__(self, pointA, pointB, pointC):
+        self.curve = FourPointCurve(pointA, pointB, pointB, pointC)
 
     def getcurvepoint(self, t):
         return self.curve.getcurvepoint(t)
