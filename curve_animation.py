9e172420ad1e4182d48e3a5827951cdbd4e66b21
Connor Marble <MarbleConor@gmail.com>
2014-10-01 10:50:44 -0400

    fixed curve animation module to comply with PEP8

--- curve_animation.py
+++ curve_animation.py
@@ -1,27 +1,26 @@
 from Tkinter import *
 from bcurves import ThreePointCurve
 
+
 class bezieranimation:
     def __init__(self, **kwargs):
-        self.pointA = kwargs.get('pointA', [0,0])
-        self.pointB = kwargs.get('pointB', [100,0])
-        self.pointC = kwargs.get('pointC',[100,100])
-        self.pointD = kwargs.get('pointD', [0,100])
+        self.pointA = kwargs.get('pointA', [0, 0])
+        self.pointB = kwargs.get('pointB', [100, 0])
+        self.pointC = kwargs.get('pointC', [100, 100])
+        self.pointD = kwargs.get('pointD', [0, 100])
 
         self.setpsize = kwargs.get('stepsize', 0.04)
-        self.time = 0
-
-
+        self.step = 0
 
 master = Tk()
 
-canvas = Canvas(master, width = 200, height = 200)
+canvas = Canvas(master, width=200, height=200)
 
 canvas.pack()
 
-canvas.create_line(0,0,512,512)
+canvas.create_line(0, 0, 512, 512)
 
-curve = ThreePointCurve([0,0], [200,0], [200,200])
+curve = ThreePointCurve([0, 0], [200, 0], [200, 200])
 
 points = []
 
@@ -29,6 +28,7 @@ for i in range(10):
     points.append(curve.getcurvepoint(float(i)/10))
 
 for i in range(len(points)-1):
-    canvas.create_line(points[i][0], points[i][1], points[i+1][0], points[i+1][1])
+    canvas.create_line(
+        points[i][0], points[i][1], points[i+1][0], points[i+1][1])
 
 canvas.mainloop()
