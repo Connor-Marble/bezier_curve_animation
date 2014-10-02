c693f40ff0536a318c3eeef54c971bd387d83c65
Connor-Marble <MarbleConnor@gmail.com>
2014-10-02 10:20:19 -0400

    added method to draw curve base lines

--- curve_animation.py
+++ curve_animation.py
@@ -33,6 +33,25 @@ class BezierAnimation:
                 curve_points[i+1][0],
                 curve_points[i+1][1])
 
+    def drawoutline(self):
+        
+        self.canvas.create_line(self.pointA[0],
+                                self.pointA[1],
+                                self.pointB[0],
+                                self.pointB[1])
+
+        if pointD is None:
+            self.canvas.create_line(self.pointB[0],
+                                    self.pointB[1],
+                                    self.pointC[0],
+                                    self.pointC[1])
+
+        else:
+            self.canvas.create_line(self.pointC[0],
+                                    self.pointC[1],
+                                    self.pointD[0],
+                                    self.pointD[1])
+
 master = Tk()
 
 canvas = Canvas(master, width=200, height=200)
