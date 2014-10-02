ed6e2bb364da5e72a9ae7a87f1e68bf2d8bd1c40
Connor-Marble <MarbleConnor@gmail.com>
2014-10-01 21:32:59 -0400

    moved curve drawing to a function in the bexier animation class

--- curve_animation.py
+++ curve_animation.py
@@ -1,16 +1,37 @@
 from Tkinter import *
-from bcurves import ThreePointCurve
+from bcurves import ThreePointCurve, FourPointCurve
 
 
 class bezieranimation:
-    def __init__(self, **kwargs):
+    def __init__(self, canvas, **kwargs):
         self.pointA = kwargs.get('pointA', [0, 0])
         self.pointB = kwargs.get('pointB', [100, 0])
         self.pointC = kwargs.get('pointC', [100, 100])
-        self.pointD = kwargs.get('pointD', [0, 100])
+        self.pointD = kwargs.get('pointD', None)
+        
+        if self.pointD is not None:
+            self.curve = FourPointCurve(
+                self.pointA, self.pointB, self.pointC, self.pointD)
+        else:
+            self.curve = ThreePointCurve(
+                self.pointA, self.pointB, self.pointC)
 
-        self.setpsize = kwargs.get('stepsize', 0.04)
+        self.stepsize = 1/float(kwargs.get('steps', 30))
         self.step = 0
+        self.canvas = canvas
+
+    def drawcurve(self):
+        curvepoints = []
+
+        for i in range(self.step+1):
+            curvepoints.append(self.curve.getcurvepoint(self.stepsize * i))
+
+        for i in range(len(curvepoints)-1):
+            self.canvas.create_line(
+                curvepoints[i][0],
+                curvepoints[i][1],
+                curvepoints[i+1][0],
+                curvepoints[i+1][1])
 
 master = Tk()
 
@@ -18,17 +39,9 @@ canvas = Canvas(master, width=200, height=200)
 
 canvas.pack()
 
-canvas.create_line(0, 0, 512, 512)
 
-curve = ThreePointCurve([0, 0], [200, 0], [200, 200])
+curve = bezieranimation(canvas)
+curve.step = 30
 
-points = []
-
-for i in range(10):
-    points.append(curve.getcurvepoint(float(i)/10))
-
-for i in range(len(points)-1):
-    canvas.create_line(
-        points[i][0], points[i][1], points[i+1][0], points[i+1][1])
-
+curve.drawcurve()
 canvas.mainloop()
