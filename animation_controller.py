5c954468401ee80958c24caa3cd5bffabdf311dc
Connor Marble <Marbleconnor@gmail.com>
2014-10-06 10:39:54 -0400

    Formatting fix

--- animation_controller.py
+++ animation_controller.py
@@ -3,6 +3,7 @@ import curve_animation as CA
 import Tkinter
 import thread
 
+
 class CurveDrawAnimation:
     def __init__(self, **kwargs):
         master = Tkinter.Tk()
@@ -10,15 +11,14 @@ class CurveDrawAnimation:
 
         self.canvas.pack()
 
-
         curve = CA.BezierAnimation(self.canvas,
-                                pointA=[200, 5],
-                                pointB=[200, 395],
-                                pointC=[5, 200],
-                                pointD=[395, 200],
-                                steps=60)
+                                   pointA=[200, 5],
+                                   pointB=[200, 395],
+                                   pointC=[5, 200],
+                                   pointD=[395, 200],
+                                   steps=60)
 
-        thread.start_new(self.animateloop,(curve, 2, kwargs))
+        thread.start_new(self.animateloop, (curve, 2, kwargs))
         
         self.canvas.mainloop()
 
@@ -35,11 +35,11 @@ class CurveDrawAnimation:
         
             time.sleep(seconds/stepcount)
 
-            if kwargs.get('persist',False) is False:
+            if kwargs.get('persist', False) is False:
                 self.canvas.delete("all")
 
         self.canvas.delete("all")
-        thread.start_new(self.animateloop,(curve, 2, kwargs))
+        thread.start_new(self.animateloop, (curve, 2, kwargs))
 
 
 if __name__ == '__main__':
